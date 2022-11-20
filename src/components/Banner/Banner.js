7a9aa18dd09da91d2a1392ca4cea7dd3aa0a1c29
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-20 23:17:44 +0600

    Navbar added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Img from "../../Images/quiz.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <img className="bannerImg" src={Img} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
