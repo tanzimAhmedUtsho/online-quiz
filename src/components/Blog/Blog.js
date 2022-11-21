697f79f73c5c614370358abefc7336f83c4d9641
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-21 21:07:54 +0600

    blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Blog = () => {
   return (
     <div>
-      <h2>Blog</h2>
+      <Card border="primary" style={{ width: "100%" }}>
+        <Card.Header>Blog-01</Card.Header>
+        <Card.Body>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+
+      <Card border="secondary" style={{ width: "100%" }}>
+        <Card.Header>Blog-02</Card.Header>
+        <Card.Body>
+          <Card.Title>Secondary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+
+      <Card border="success" style={{ width: "100%" }}>
+        <Card.Header>Blog-03</Card.Header>
+        <Card.Body>
+          <Card.Title>Success Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
